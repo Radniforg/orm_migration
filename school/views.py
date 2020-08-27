52674185f0e910ba155990455d46051d3f310fb8
Ivan Alexandrov <radniforg@yandex.ru>
2020-08-27 18:17:07 +0300

    complete

--- school/views.py
+++ school/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import render
 
 from .models import Student
 
+class StudentsListView(ListView):
 
-def students_list(request):
+    model = Student
     template = 'school/students_list.html'
-    context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
-    return render(request, template, context)
+
+
+
+
+# def students_list(request):
+#     template = 'school/students_list.html'
+#     context = {}
+#
+#     # используйте этот параметр для упорядочивания результатов
+#     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+#     ordering = 'group'
+#
+#     return render(request, template, context)
